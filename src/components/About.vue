96dc3645827d0e18eee0ac36beac625c2f88595a
Jonerie 123 hehehe <154795227+Joneriehahahaha@users.noreply.github.com>
2024-04-15 20:22:11 +0800

    Add files via upload

---
+++ src/components/About.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex flex-col items-center min-h-screen">
+    <div class="text-center">
+      <div class="mx-auto mb-4 mt-20 md:mt-32 h-48 w-48 md:h-64 md:w-64 overflow-hidden rounded-full bg-gradient-to-tr from-orange-500 to-purple-700">
+        <img src="/images/brigoli.png" alt="" class="w-full h-full object-cover" />
+      </div>
+      <h1 class="max-w-screen-sm text-4xl font-extrabold text-black mb-8">
+         <br />
+         <span class="text-transparent bg-clip-text bg-gradient-to-tr from-orange-500 to-purple-700"> </span>
+      </h1>
+      <p class="mx-auto mb-16 text-lg font-lg font-semibold text-black max-w-lg">Hello, I`m Klien Brigoli, and I`m a third-year college student pursuing a Bachelor of Science in Information Technology
+      </p>
+      
+      <p class="mx-auto mb-16 text-lg font-lg font-semibold text-black max-w-lg">At Cordova Public College, I am constantly learning and honing my skills through hands-on projects, coursework, and collaboration with peers and instructors. I believe in the importance of staying updated with the latest advancements in technology and actively seek opportunities to expand my knowledge and expertise.
+      </p>
+      <div class="text-center">
+        <button class="text-gray-800 inline-block px-8 py-4 mr-4 bg-blue-400 rounded-full font-semibold text-lg">Get In Touch</button>
+        <button class="text-black inline-block px-8 py-4 bg-transparent border-2 border-black rounded-full font-semibold text-lg">Download CV</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // Component logic
+};
+</script>
+
+<style scoped>
+@media (min-width: 640px) {
+  .h-24 {
+    height: 6rem; /* Adjust the height for smaller screens */
+  }
+  .w-24 {
+    width: 6rem; /* Adjust the width for smaller screens */
+  }
+}
+</style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="bg-slate-900">
+
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+      
+        <h2 class="text-3xl font-extrabold text-black">Contact Information</h2>
+        <p class="mt-4 text-lg text-gray-700 font-bold">
+          .
+        </p>
+        
+      
+      
+      
+            
+         
+            <div class="flex justify-center"></div>
+            <div class="max-w-full mx-auto rounded-lg overflow-hidden">
+              <div class="px-6 py-4">
+                <h3 class="text-lg font-bold text-black-400">My Address</h3>
+                <p class="mt-1 text-gray-800">
+                  Pusok Lapulapu City
+                </p>
+
+                
+              </div>
+              <div class="border-t border-gray-200 px-6 py-4">
+                <h5 class="text-lg font-bold text-black-400">Social Links</h5>
+                <p class="mt-1 text-black font-bold">
+                  Facebook:
+                  <span class="font-normal bold"
+                    >https://www.facebook.com/Klienbrigoli</span
+                  >
+                </p>
+                <p class="mt-1 text-black font-bold">
+                  Instagram:
+                  <span class="font-normal bold">https://www.instagram.com/Klienbrigoli/</span>
+                </p>
+                <p class="mt-1 ttext-black font-bold">
+                  Twitter: <span class="font-normal">https://twitter.com/Klienbrigoli/</span>
+                </p>
+              </div>
+              <div class="border-t border-gray-200 px-6 py-4">
+                <h3 class="text-lg text-black font-semibold font-sans">
+                  Contact
+                </h3>
+                <p class="mt-1 text-black font-bold">
+                  Email: <span class="font-normal">Klienbrigoli@gmail.com</span>
+                </p>
+                <p class="mt-1 text-black font-bold">
+                  Phone: <span class="font-normal"> + 63961237723</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
